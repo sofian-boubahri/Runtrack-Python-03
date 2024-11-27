a7c9c6f923fbbd359a34921d4aabf7166fe68815
Sofian <sofianboubahri@gmail.com>
2024-11-27 09:04:25 +0100

    push

---
+++ Job 01/main.py
@@ -0,0 +1,9 @@
+valeur1 = int(input("Donne moi la première valeur :"))
+
+valeur2 = int(input("Donne moi la deuxième valeur :"))
+
+if valeur1 == valeur2:
+    print("Valeur1 est égal à valeur2")
+else:
+    print("Les deux valeurs ne sont pas égales")
+
---
+++ Job 02/main.py
@@ -0,0 +1,6 @@
+age = 17
+
+if age > 17:
+    print("Tu peux voter")
+else:
+    print("Tu ne peux pas voter")
